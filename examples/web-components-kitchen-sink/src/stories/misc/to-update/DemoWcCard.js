a8925548291dfcb15f62eca16116c860c9946a17
Gaëtan Maisse <gaetanmaisse@gmail.com>
2021-05-25 21:44:08 +0200

    example(web-components): update WC kitchen sink with stories developed for @storybook/lit

--- examples/web-components-kitchen-sink/src/stories/misc/to-update/DemoWcCard.js
+++ examples/web-components-kitchen-sink/src/stories/misc/to-update/DemoWcCard.js
@@ -1,6 +1,5 @@
-/* eslint-disable import/extensions */
 import { CustomEvent } from 'global';
-import { LitElement, html } from 'lit-element';
+import { LitElement, html } from 'lit';
 import { demoWcCardStyle } from './demoWcCardStyle.css.js';
 
 /**
